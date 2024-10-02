9893841345d8dc429b81099f20d75becf9d7a44b
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-03 00:16:48 +0300

    4. Rest parameter syntax for functions

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,8 @@
 export default function returnHowManyArguments(...nums) {
-	let total = 0;
-	for (const num of nums) {
-		total++;
-	}
-	return total;
+  let total = 0;
+  // eslint-disable-next-line no-unused-vars, no-shadow
+  for (const num of nums) {
+    total += 1;
+  }
+  return total;
 }
