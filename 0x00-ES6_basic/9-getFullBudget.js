86f127c2e172fc048bea4eb7fe015c9f77e23ba5
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-03 04:01:30 +0300

    9. ES6 method properties

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/extensions, no-shadow
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+
+  return fullBudget;
+}
