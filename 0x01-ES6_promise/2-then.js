185367df2e7d8d912ee0bfe64bdf433bb33bc5bf
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-12 11:31:54 +0300

    2. Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,6 @@
-export default function handleResponseFromAPI (promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve();
-    } else {
-      reject();
-    }
-  }).then(() => {
-    return {
-      status: 200,
-      body: 'Success'
-    };
-  }).catch(() => ({})).finally(() => {
-    console.log('Got a response from the API');
-  });
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
