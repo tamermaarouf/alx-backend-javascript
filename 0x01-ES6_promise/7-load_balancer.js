7ec39fe2a9fdb7236a7d060b12bdbc0c29c3c5ca
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-14 02:01:57 +0300

    7. Load balancer

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+
+export default async function loadBalancer (chinaDownload, USDownload) {
+  return Promise
+    .race([chinaDownload, USDownload])
+    .then((value) => { console.log(value); });
+}
