0d1e04841a269f2ab206c6f7f13374343d743bee
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-13 07:38:03 +0300

    3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  const promise = Promise.all([uploadPhoto(), createUser()]);
-  promise
-    .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`));
+export default function handleProfileSignup () {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }
