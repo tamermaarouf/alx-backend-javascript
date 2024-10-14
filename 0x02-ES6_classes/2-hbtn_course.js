f7228e131c1f180811c4d3578752b4b076790033
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-14 20:45:40 +0300

    2. A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,25 +1,25 @@
 export default class HolbertonCourse {
-  constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+  constructor (name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  get name() { return this._name; }
+  get name () { return this._name; }
 
-  set name(name) {
+  set name (name) {
     if ((typeof name) !== 'string') { throw new TypeError('Name must be a string'); }
     this._name = name;
   }
 
-  get length() { return this._length; }
-
-  set length(length) {
-    if (typeof length !== 'number') { throw new TypeError('Length must be a number'); }
+  set length (length) {
+    if ((typeof length) !== 'number') { throw new TypeError('Length must be a number'); }
     this._length = length;
   }
 
-  get students() { return this._students; }
+  get length () { return this._length; }
 
-  set students(students) { this._student = students; }
+  get students () { return this._students; }
+
+  set students (students) { this._student = students; }
 }
