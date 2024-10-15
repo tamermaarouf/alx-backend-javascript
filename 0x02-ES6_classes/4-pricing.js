1bea422e327638a4697dc9b602f89eace0777994
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-15 18:01:09 +0300

    4. Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,20 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor (amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount () { return this._amount; }
+
+  set amount (setAmount) { this._amount = setAmount; }
+
+  get currency () { return this.currency; }
+
+  set currency (setCurrency) { this._currency = setCurrency; }
+
+  displayFullPrice () {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+}
