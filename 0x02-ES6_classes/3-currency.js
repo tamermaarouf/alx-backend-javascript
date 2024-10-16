4e46d9017ba522d9c736f6dc03650b881c1a510a
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-16 03:05:45 +0300

    Merge

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -17,24 +17,3 @@ export default class Currency {
     return `${this._name} (${this._code})`;
   }
 }
-=======
-	constructor(code, name){
-		this.code = code;
-		this.name = name;
-	}
-
-	get code() {
-		return this._code;
-	}
-	set code(setCode){
-		this._code = setCode;
-	}
-
-	get name() {
-		return this._name;
-	}
-	set name(setName) {
-		this._name = setName;
-	}
-}
->>>>>>> d4970e0 ( 5. A Building)
