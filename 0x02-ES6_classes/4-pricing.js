6ee9325d88405b3ba79986ad13c0130379e44dfa
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-16 04:01:20 +0300

    4. Pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,23 +1,25 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-  constructor (amount, currency) {
+  constructor(amount, currency) {
     this.amount = amount;
     this.currency = currency;
   }
 
-  get amount () { return this._amount; }
+  get amount() { return this._amount; }
 
-  set amount (setAmount) { this._amount = setAmount; }
+  set amount(setAmount) { this._amount = setAmount; }
 
-  get currency () { return this.currency; }
+  get currency() { return this.currency; }
 
-  set currency (setCurrency) { this._currency = setCurrency; }
+  set currency(setCurrency) { this._currency = setCurrency; }
 
-  displayFullPrice () {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  displayFullPrice() {
+    const cur = new Currency(this._currency.code, this._currency.name).displayFullCurrency();
+    return `${this._amount} ${cur}`;
   }
+
   static convertPrice(amount, conversionRate) {
-	  return (amount * conversionRate);
+    return (amount * conversionRate);
   }
 }
