74588935170a14970ad56c79367926e48fe6d25c
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-16 04:58:55 +0300

    5. A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,2 +1,11 @@
 export default class Building {
+	constructor(sqft){
+		this.sqft = sqft;
+	}
+
+	get sqft () { return this._sqft; }
+	set sqft (setSqft) { this._sqft = setSqft; }
+	evacuationWarningMessage () {
+		throw new Error('Class extending Building must override evacuationWarningMessage');
+	}
 }
