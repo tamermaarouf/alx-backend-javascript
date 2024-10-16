73a51db2465ff68a5fb9e6a02afdd4bd2f3e9544
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-16 03:02:10 +0300

    5. A Building

--- 0x02-ES6_classes/4-main.js
+++ 0x02-ES6_classes/4-main.js
@@ -1,12 +1,7 @@
 import Pricing from './4-pricing.js';
 import Currency from './3-currency.js';
 
-<<<<<<< HEAD
-const p = new Pricing(100, new Currency('EUR', 'Euro'));
-console.log(p);
-console.log(p.displayFullPrice());
-=======
+
 const p = new Pricing(100, new Currency("EUR", "Euro"))
 console.log(p);
 console.log(p.displayFullPrice());
->>>>>>> d4970e0 ( 5. A Building)
