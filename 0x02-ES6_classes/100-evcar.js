c05babc9d31f4b01c61c966e2dcc4e6b614e2887
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-17 17:26:34 +0300

    11. EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,12 +1,12 @@
 import Car from './10-car';
 
 export default class EVCar extends Car {
-  constructor (brand, motor, color, range) {
+  constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  cloneCar () {
+  cloneCar() {
     /* eslint new-cap: ["error", { "newIsCap": false  }] */
     const newClone = super.constructor[Symbol.species];
     return new newClone();
