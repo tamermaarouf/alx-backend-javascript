4b52426215ec6d18415b80f2910d855cc5bbf8ec
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-20 07:53:00 +0300

    5. Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,6 +5,9 @@ length (Number), position (Number), and value (Number).
 If adding the value is not possible the error Position outside range should be thrown. */
 
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
   const buffer = new ArrayBuffer(length);
   const dv = new DataView(buffer);
   dv.setInt8(position, value);
