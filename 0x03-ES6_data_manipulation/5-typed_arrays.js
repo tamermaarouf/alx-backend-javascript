ea75b45b21819ed48b2a8dfced6fd89a9614637a
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-20 07:47:08 +0300

    5. Typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+/* Create a function named createInt8TypedArray
+that returns a new ArrayBuffer with an Int8 value at a specific position.
+It should accept three arguments:
+length (Number), position (Number), and value (Number).
+If adding the value is not possible the error Position outside range should be thrown. */
+
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const dv = new DataView(buffer);
+  dv.setInt8(position, value);
+  return dv;
+}
