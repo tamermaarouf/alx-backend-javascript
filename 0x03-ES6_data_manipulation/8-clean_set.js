0da5105d5879ef75fd9b6747036834176ac0e06b
tamer_maarouf <tamermaarouf@gmail.com>
2024-10-21 08:38:15 +0300

    8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,15 +6,16 @@
  * */
 
 export default function cleanSet(mySet, str) {
-  if (!mySet && !str && !(mySet instanceof Set) && typeof str !== 'string') return '';
   const newItem = [];
-  if (str.length !== 0) {
-    const values = mySet.values();
-    for (const value of values) {
-      if (typeof value === 'string' && value.startsWith(str)) {
-        const word = value.split(str)[1];
-        newItem.push(word);
-      }
+  if ((!mySet) || (!str) || !(mySet instanceof Set)
+|| (typeof str !== 'string') || (str.length === 0)) {
+    return '';
+  }
+  const values = mySet.values();
+  for (const value of values) {
+    if (typeof value === 'string' && value.startsWith(str)) {
+      const word = value.split(str)[1];
+      newItem.push(word);
     }
   }
   return newItem.join('-');
